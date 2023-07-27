cf48665e268ea69eefaba9c8b9feb1078b386c49
Michael0520 <po2319987@gmail.com>
2023-07-28 03:19:54 +0800

    feat: 🎸 add props & searchParams to render pagination

--- components/PaginationControls.tsx
+++ components/PaginationControls.tsx
@@ -1,13 +1,48 @@
 "use client"
 
+import { FC } from "react"
+import { useSearchParams } from "next/navigation"
+
 import { Button } from "./ui/button"
 
-const PaginationControls = () => {
+interface PaginationControlsProps {
+  hasNextPage: boolean
+  hasPrevPage: boolean
+}
+
+const PaginationControls: FC<PaginationControlsProps> = ({
+  hasNextPage,
+  hasPrevPage,
+}) => {
+  const searchParams = useSearchParams()
+
+  const page = searchParams.get("page") ?? "1"
+  const per_page = searchParams.get("per_page") ?? "5"
+
+  const handleClickToPrevPage = () => {}
+  const handleClickToNextPage = () => {}
+
   return (
-    <div className="flex w-full items-center justify-center gap-4">
-      <Button>Prev</Button>
-      <div>pages</div>
-      <Button>Next</Button>
+    <div className="flex gap-4">
+      <Button
+        className="bg-blue-500 p-1 text-white"
+        disabled={!hasPrevPage}
+        onClick={handleClickToPrevPage}
+      >
+        prev page
+      </Button>
+
+      <div>
+        {page} / {Math.ceil(10 / Number(per_page))}
+      </div>
+
+      <Button
+        className="bg-blue-500 p-1 text-white"
+        disabled={!hasNextPage}
+        onClick={handleClickToNextPage}
+      >
+        next page
+      </Button>
     </div>
   )
 }
