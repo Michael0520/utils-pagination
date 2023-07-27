67697ce7a95ee9319d39d900db9b36c59503e66f
Michael0520 <po2319987@gmail.com>
2023-07-28 03:47:02 +0800

    feat: 🎸 add queryProvier and defaultSetting

--- components/client/Providers/QueryProvider.tsx
+++ components/client/Providers/QueryProvider.tsx
@@ -1,12 +1,18 @@
 "use client"
 
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+import { queryClientOptions } from "@/config/constants"
 
 interface QueryProvidersProp {
   children: ReactNode
 }
 const QueryProvider = ({ children }: QueryProvidersProp) => {
-  return <div>{children}</div>
+  const [queryClient] = useState(() => new QueryClient(queryClientOptions))
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
 
 export default QueryProvider
