2eaa6d60e6a478f1a653e1707f3264be1122565e
Michael0520 <po2319987@gmail.com>
2023-07-28 03:16:28 +0800

    feat: 🎸 add adjust currentPage to render jsx from router-path

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,33 @@ import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import PaginationControls from "@/components/PaginationControls"
 
-export default function IndexPage() {
+const data = [
+  "product 1",
+  "product 2",
+  "product 3",
+  "product 4",
+  "product 5",
+  "product 6",
+  "product 7",
+  "product 8",
+  "product 9",
+  "product 10",
+]
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const currentPage = searchParams["page"] ?? "1"
+  const itemsPerPage = searchParams["per_page"] ?? "5"
+
+  // mocked, skipped and limited in the real app
+  const startIndex = (Number(currentPage) - 1) * Number(itemsPerPage) // 0, 5, 10 ...
+  const endIndex = startIndex + Number(itemsPerPage) // 5, 10, 15 ...
+
+  const productList = data.slice(startIndex, endIndex)
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
@@ -35,8 +61,17 @@ export default function IndexPage() {
           GitHub
         </Link>
       </div>
-      {/* pagination */}
-      <PaginationControls />
+      {/* pagination example */}
+      <div className="flex flex-col items-center gap-2">
+        {productList.map((product) => (
+          <p key={product}>{product}</p>
+        ))}
+
+        <PaginationControls
+          hasNextPage={endIndex < data.length}
+          hasPrevPage={startIndex > 0}
+        />
+      </div>
     </section>
   )
 }
