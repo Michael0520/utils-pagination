7015940af9193610e211cf677375826b586676d8
Michael0520 <po2319987@gmail.com>
2023-08-04 04:16:25 +0800

    feat: 🎸 update api domain & list UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,22 @@
-import Link from "next/link"
+import Image from "next/image"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
 import PaginationControls from "@/components/PaginationControls"
 
-const data = [
-  "product 1",
-  "product 2",
-  "product 3",
-  "product 4",
-  "product 5",
-  "product 6",
-  "product 7",
-  "product 8",
-  "product 9",
-  "product 10",
-]
+const getPicture = async () => {
+  const res = await fetch(`https://picsum.photos/v2/list`)
+  return res.json()
+}
 
-export default function Home({
+type productDetail = {
+  id: string
+  author: string
+  width: number
+  height: number
+  url: string
+  download_url: string
+}
+
+export default async function Home({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
@@ -29,47 +28,51 @@ export default function Home({
   const startIndex = (Number(currentPage) - 1) * Number(itemsPerPage) // 0, 5, 10 ...
   const endIndex = startIndex + Number(itemsPerPage) // 5, 10, 15 ...
 
-  const productList = data.slice(startIndex, endIndex)
+  const rawData = await getPicture()
+  const artistData = await getPicture().then((res) => {
+    return res.slice(startIndex, endIndex)
+  })
 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Utils pagination <br className="hidden sm:inline" />
+          easily build pagination for prev & next page function
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           Accessible and customizable components that you can copy and paste
           into your apps. Free. Open Source. And Next.js 13 Ready.
         </p>
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
       {/* pagination example */}
       <div className="flex flex-col items-center gap-2">
-        {productList.map((product) => (
-          <p key={product}>{product}</p>
+        {artistData.map((product: productDetail) => (
+          <div
+            key={product.id}
+            className="w-112 mb-4 flex flex-col items-center rounded-md bg-white p-4 shadow-md"
+          >
+            <p className="mb-2 flex w-full items-center justify-between text-slate-700">
+              <span>ID: {Number(product.id) + 1}</span>
+              <span>Name: {product.author}</span>
+            </p>
+            <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-md">
+              <Image
+                src={product.download_url}
+                alt="image"
+                width={500}
+                height={Math.round((500 / product.width) * product.height)}
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </div>
+          </div>
         ))}
 
         <PaginationControls
-          hasNextPage={endIndex < data.length}
+          hasNextPage={endIndex < rawData.length}
           hasPrevPage={startIndex > 0}
+          totalSize={rawData.length}
         />
       </div>
     </section>
